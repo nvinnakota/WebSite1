049ff3a872cf20fb4b7226f2f2b621747ecfc39c
nvinnakota <v.nithya789@gmail.com>
2013-09-22 23:57:12 -0700

    first commit

---
+++ Login.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("usr_login", con))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add(new SqlParameter("@username", txtusername.Text));
+                    command.Parameters.Add(new SqlParameter("@password", txtpassword.Text));
+                    var usr_id = command.ExecuteScalar();
+                    if (usr_id != null)
+                    {
+                        Session["usr_id"] = usr_id.ToString();
+                        Session["UserName"] = txtusername.Text;
+                        Response.Redirect("AddCompany.aspx");
+
+
+                    }
+                    else
+                    {
+                        lblError.Visible = true;
+                    }
+
+
+                }
+
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
+
+
+
+
+    }
+}
